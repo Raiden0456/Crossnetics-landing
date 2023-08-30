f55762ddb4ba6985dd02977b64017881ef47623b
Vrezh Fedora <vrezhraiden@gmail.com>
2023-08-30 12:23:09 +0200

    feat(hero): added ipad to hero section

--- crossnetics/src/components/Ipad_swiper.jsx
+++ crossnetics/src/components/Ipad_swiper.jsx
@@ -1,5 +1,5 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCreative, Pagination } from 'swiper/modules';
+import { EffectCreative, Autoplay } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/effect-creative';
@@ -7,27 +7,35 @@ import 'swiper/css/pagination';
 
 export default function IpadSwiper() {
   return (
-    <div class="absolute top-5 left-6 w-11/12 h-11/12 drop-shadow-ipad rounded-lg overflow-hidden">
-      <Swiper 
-        grabCursor={true}
-        effect={'creative'}
-        creativeEffect={{
-          prev: {
-            shadow: true,
-            translate: ['-20%', 0, -1],
-          },
-          next: {
-            translate: ['100%', 0, 0],
-          },
-        }}
-        pagination={true}
-        modules={[EffectCreative, Pagination]}
-        className="ipadSwiper"
-        >
-        <SwiperSlide><img src='/slide.png' /></SwiperSlide>
-        <SwiperSlide><img src='/slide.png' /></SwiperSlide>
-        <SwiperSlide><img src='/slide.png' /></SwiperSlide>
-      </Swiper>
+    <div class="absolute top-5 left-6 w-11/12 h-11/12 drop-shadow-ipad bg-[url('/ipad.png')] bg-cover">
+      <div class="relative w-full h-full p-2 lg:p-3 xl:p-4">
+        <div class="w-full h-full rounded-lg overflow-hidden">
+          <Swiper 
+            grabCursor={true}
+            effect={'creative'}
+            creativeEffect={{
+              prev: { 
+                shadow: true,
+                translate: ['-20%', 0, -1],
+              },
+              next: {
+                translate: ['100%', 0, 0],
+              },
+            }}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            resistance={true}
+            resistanceRatio={0}
+            modules={[Autoplay, EffectCreative]}
+          >
+            <SwiperSlide><img src='/slide.png' /></SwiperSlide>
+            <SwiperSlide><img src='/slide.png' /></SwiperSlide>
+            <SwiperSlide><img src='/slide.png' /></SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
     </div>
   );
 }
