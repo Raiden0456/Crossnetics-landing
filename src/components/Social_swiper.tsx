d36c5600271bd89efeedb262737952bf8c9d3de0
Vrezh Fedora <vrezhraiden@gmail.com>
2023-09-09 18:15:52 +0200

    style(text): updates to text and text styling

--- src/components/Social_swiper.tsx
+++ src/components/Social_swiper.tsx
@@ -56,7 +56,7 @@ export default function SocialSwiper({ social: social }: { social: Social[] }) {
                     <div className="relative flex flex-col gap-y-10 md:gap-y-12 items-start justify-center lg:w-7/12">
                       <div className="relative flex flex-col gap-y-4 justify-start items-start">
                         <h1 className="text-white text-2xl xl:text-3xl 2xl:text-4xl font-bold capitalize">{entry.title}</h1>
-                        <p className="text-white text-lg xl:text-xl 2xl:text-2xl font-medium whitespace-pre-line">{entry.description}</p>
+                        <p className="text-white text-lg xl:text-xl 2xl:text-2xl font-medium whitespace-pre-line leading-tight">{entry.description}</p>
                       </div>
                       <div className="flex flex-col gap-y-2 justify-start items-start">
                         <img src="/logo.svg" alt="logo" className="w-10" loading='lazy'/>
