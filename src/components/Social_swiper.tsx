8ded5572a5d1498059877cc51477eece3474d139
Vrezh Fedora <vrezhraiden@gmail.com>
2023-09-11 13:13:38 +0200

    style(social): Social swiper and text updates

--- src/components/Social_swiper.tsx
+++ src/components/Social_swiper.tsx
@@ -7,7 +7,9 @@ import 'swiper/css/effect-fade';
 import '../styles/Social_swiper.css' 
 
 interface Social {
+  header: string;
   title: string;
+  button: string;
   description: string;
   testimonial: string;
   image: string;
@@ -29,7 +31,7 @@ export default function SocialSwiper({ social: social }: { social: Social[] }) {
         const textColorClass = isActive ? "text-blue-500" : "text-white";
         return `
           <div class="${baseClasses} ${bgColorClass} ${bgHoverClass}" onClick="document.querySelector('.custom-swiper').swiper.slideTo(${index})">
-            <div class="${textColorClass} text-xs lg:text-3xs xl:text-xs font-extrabold uppercase">${entry.title.split(' ')[0]}</div>
+            <div class="${textColorClass} text-xs lg:text-3xs xl:text-xs font-extrabold uppercase">${entry.button}</div>
           </div>`;
       }).join("");
     }
@@ -48,19 +50,20 @@ export default function SocialSwiper({ social: social }: { social: Social[] }) {
             
             return (
               <SwiperSlide key={index}>
-                <div style={{background: `linear-gradient(to left, ${entry.gradient[0]}, ${entry.gradient[1]})`}} className="h-fit min-h-[50rem] md:min-h-[40rem] lg:min-h-[650px] 2lg:min-h-[550px] xl:min-h-[700px] flex items-center justify-center">
-                  <div className="relative flex flex-row justify-between w-11/12 lg:w-2/3 h-full mt-48 md:mt-0 2lg:mt-10 xl:mt-5">
+                <div style={{background: `linear-gradient(to left, ${entry.gradient[0]}, ${entry.gradient[1]})`}} className="h-[50rem] md:h-[40rem] lg:h-[650px] 2lg:h-[550px] xl:h-[700px] flex items-center justify-center">
+                  <div className="relative flex flex-row justify-between w-11/12 lg:w-2/3 h-2/3 mt-48 md:mt-0 2lg:mt-10 xl:mt-5">
                     <div className="2lg:flex w-4/12 items-center absolute hidden  2lg:relative">
                       <img src={`/3d_images/${entry.image}`} alt="3d-image" className="w-full" loading='lazy'/>
                     </div>
-                    <div className="relative flex flex-col gap-y-10 md:gap-y-12 items-start justify-center lg:w-7/12">
+                    <div className="relative flex flex-col gap-y-6 items-start justify-between h-full lg:w-7/12">
                       <div className="relative flex flex-col gap-y-4 justify-start items-start">
-                        <h1 className="text-white text-2xl xl:text-3xl 2xl:text-4xl font-bold capitalize">{entry.title}</h1>
-                        <p className="text-white text-lg xl:text-xl 2xl:text-2xl font-medium whitespace-pre-line leading-tight">{entry.description}</p>
+                        <h1 className="text-white text-xl xl:text-2xl 2xl:text-3xl font-bold leading-tight">{entry.header}</h1>
+                        <h2 className="text-white text-lg xl:text-xl 2xl:text-2xl font-medium leading-tight">{entry.title}</h2>
+                        <p className="text-sky-100 text-xs xl:text-base whitespace-pre-line leading-[21px]">{entry.description}</p>
                       </div>
                       <div className="flex flex-col gap-y-2 justify-start items-start">
-                        <img src="/logo.svg" alt="logo" className="w-10" loading='lazy'/>
-                        <p className="text-sky-100 text-2xs xl:text-sm whitespace-pre-line">{entry.testimonial}</p>
+                        <img src="/logo.svg" alt="logo" className="w-8" loading='lazy'/>
+                        <p className="text-sky-100 text-xs xl:text-base whitespace-pre-line">{entry.testimonial}</p>
                       </div>
                     </div>
                     <div className="lg:flex w-4/12 items-center hidden lg:relative 2lg:hidden 2lg:absolute">
