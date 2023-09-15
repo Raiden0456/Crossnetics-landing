8e9f6d8c8b54bee7c36b7bee95c4e52afdc07455
Vrezh Fedora <vrezhraiden@gmail.com>
2023-09-15 18:43:47 +0200

    refactor(collapse): remade navigation logic

--- src/components/Collapse.tsx
+++ src/components/Collapse.tsx
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-
+import { useState, useEffect, useRef, useCallback, useMemo } from "react";
 interface Details {
   title: string;
   description: string;
@@ -14,6 +13,7 @@ interface Sectione {
     testimonial?: string;
   };
 }
+
 interface Props {
   sections: Sectione[];
 }
@@ -23,7 +23,44 @@ Collapse.defaultProps = {
 };
 
 export default function Collapse({ sections }: Props) {
+  const headerHeight = 100;
+  const initialOpenSections = useMemo(() => new Array(sections.length).fill(false), [sections.length]);
+  const [openSections, setOpenSections] = useState<boolean[]>(initialOpenSections);
+
   const [activeSection, setActiveSection] = useState<number | null>(null);
+  const sectionRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  const updateActiveSection = useCallback(() => {
+    const viewportHeight = window.innerHeight;
+    const offset = headerHeight;
+    let activeIndex = null;
+
+    for (let i = 0; i < sectionRefs.current.length; i++) {
+      const section = sectionRefs.current[i];
+      if (section) {
+        const rect = section.getBoundingClientRect();
+        if (rect.top < viewportHeight - offset && rect.bottom > offset) {
+          activeIndex = i;
+          break;
+        }
+      }
+    }
+
+    setActiveSection(activeIndex);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", updateActiveSection);
+    return () => window.removeEventListener("scroll", updateActiveSection);
+  }, []);
+
+  const scrollToSection = useCallback((index: number) => {
+    const section = sectionRefs.current[index];
+    if (section) {
+      const offsetTop = section.offsetTop - headerHeight;
+      window.scrollTo({ top: offsetTop, behavior: "smooth" });
+    }
+  }, []);
 
   return (
     <div className="bg-gradient-to-b from-sky-200 to-sky-100 flex items-center justify-center">
@@ -35,12 +72,13 @@ export default function Collapse({ sections }: Props) {
                 key={index}
                 className={`flex flex-row gap-x-2 rounded-lg items-start justify-start transition-all duration-300 cursor-pointer ${
                   activeSection === index
-                    ? "text-white bg-blue-500  ml-px p-2 w-fit scale-105"
+                    ? "text-white bg-blue-500 ml-px p-2 w-fit scale-105"
                     : "text-gray-600 hover:scale-105"
                 }`}
-                onClick={() =>
-                  setActiveSection(activeSection === index ? null : index)
-                }
+                onClick={() => {
+                  scrollToSection(index);
+                  updateActiveSection();
+                }}
               >
                 <p
                   className={`${
@@ -57,12 +95,19 @@ export default function Collapse({ sections }: Props) {
           </div>
           <div className="flex flex-col gap-y-6 w-full">
             {(sections || []).map((item, index) => (
-              <div key={index} className="flex flex-col gap-y-6 w-full">
+              <div
+                key={index}
+                className="flex flex-col gap-y-6 w-full"
+                ref={(el) => (sectionRefs.current[index] = el)}
+              >
                 <div
-                  className="flex flex-row justify-between cursor-pointer w-full"
-                  onClick={() =>
-                    setActiveSection(activeSection === index ? null : index)
-                  }
+                  className="flex flex-row justify-between cursor-pointer w-full gap-6"
+                  onClick={() => {
+                    const newOpenSections = [...openSections];
+                    newOpenSections[index] = !newOpenSections[index];
+                    setOpenSections(newOpenSections);
+                    updateActiveSection();
+                  }}
                 >
                   <div className="flex flex-row gap-x-4">
                     <p className="text-blue-500 font-bold text-xl xl:text-3xl">
@@ -74,7 +119,7 @@ export default function Collapse({ sections }: Props) {
                   </div>
                   <img
                     className={`transform duration-500 ${
-                      activeSection === index ? "-scale-y-100" : ""
+                      openSections[index] ? "-scale-y-100" : ""
                     }`}
                     src="arrow.svg"
                     alt=""
@@ -84,7 +129,7 @@ export default function Collapse({ sections }: Props) {
                 </div>
                 <div
                   className={`transform transition-max-h duration-500 ease-in-out overflow-hidden ${
-                    activeSection === index
+                    openSections[index]
                       ? "max-h-[3000px] opacity-100 scale-100"
                       : "max-h-0 opacity-0 scale-95"
                   }`}
@@ -109,7 +154,11 @@ export default function Collapse({ sections }: Props) {
                         </div>
                       </div>
                     </div>
-                    <div className={`${item.key_capabilities ? "" : "hidden absolute"} flex flex-col gap-y-2`}>
+                    <div
+                      className={`${
+                        item.key_capabilities ? "" : "hidden absolute"
+                      } flex flex-col gap-y-2`}
+                    >
                       <div className="flex flex-row gap-x-4 w-full items-start justify-start">
                         <img
                           className="w-5 mt-2"
@@ -140,7 +189,11 @@ export default function Collapse({ sections }: Props) {
                         </div>
                       </div>
                     </div>
-                    <div className={`${item.benefits ? "" : "hidden absolute"} flex flex-col gap-y-2`}>
+                    <div
+                      className={`${
+                        item.benefits ? "" : "hidden absolute"
+                      } flex flex-col gap-y-2`}
+                    >
                       <div className="flex flex-row gap-x-4 w-full items-start justify-start">
                         <img
                           className="w-5 mt-2"
@@ -167,7 +220,9 @@ export default function Collapse({ sections }: Props) {
                                 </p>
                               </div>
                             ))}
-                            <p className="text-gray-600 text-xs xl:text-base italic">{item.benefits?.testimonial}</p>
+                            <p className="text-gray-600 text-xs xl:text-base italic">
+                              {item.benefits?.testimonial}
+                            </p>
                           </div>
                         </div>
                       </div>
