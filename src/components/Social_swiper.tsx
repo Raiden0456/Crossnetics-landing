d30054c0d16e36246d46e2dea3be0aaa75f89839
Vrezh Fedora <vrezhraiden@gmail.com>
2023-09-17 17:06:03 +0200

    optimization(load): general optimization updates

--- src/components/Social_swiper.tsx
+++ src/components/Social_swiper.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import SwiperCore from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectFade, Pagination } from 'swiper/modules';
