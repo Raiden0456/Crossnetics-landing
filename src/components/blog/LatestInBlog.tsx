3c16410ec82b78a3eaa156a018c5c299f9a113ee
Vrezh Fedora <vrezhraiden@gmail.com>
2023-11-15 16:23:48 +0100

    feat(blog): added filler data for blog

--- src/components/blog/LatestInBlog.tsx
+++ src/components/blog/LatestInBlog.tsx
@@ -4,20 +4,88 @@ import Skeleton from "react-loading-skeleton";
 
 import "react-loading-skeleton/dist/skeleton.css";
 
-interface BlogSkeletonProps {
-  width?: string | number;
-  height?: string | number;
-  rounded?: string | number;
+interface Post {
+  id: number;
+  date: string;
+  title: string;
+  text: string;
+  tags: string[];
+  likes: number;
+  image: string;
 }
 
-const BlogItemSkeleton = ({ width, height, rounded }: BlogSkeletonProps) => {
+interface BlogSkeletonProps {
+  height?: string | number;
+  rounded?: string | number;
+  vertical?: boolean;
+  post: Post;
+}
+
+const BlogItemSkeleton = ({
+  height,
+  rounded,
+  vertical,
+  post,
+}: BlogSkeletonProps) => {
   const style = {
-    width,
     height,
     borderRadius: rounded,
   };
-  const [loading, setLoading] = useState(true);
-  return <>{loading ? <Skeleton style={style} /> : <div>hello</div>}</>;
+  const [loading, setLoading] = useState(false);
+
+  const orientationClasses = vertical ? "flex-col" : "flex-row";
+  const orientationImageClasses = vertical
+    ? "w-full h-full object-cover"
+    : "w-48 h-full object-cover";
+
+  return (
+    <>
+      {loading ? (
+        <Skeleton style={style} />
+      ) : (
+        <div
+          className={`bg-white min-h-64 rounded-[40px] flex ${orientationClasses} overflow-hidden`}
+        >
+          <img
+            src={post.image}
+            alt={post.title}
+            className={orientationImageClasses}
+          />
+          <div className="p-6 xl:p-8 flex flex-col gap-y-2 xl:gap-y-4">
+            <div className=" text-slate-300 text-2xs font-medium">
+              {post.date}
+            </div>
+            <h1 className="text-blue-500 text-base lg:text-sm xl:text-base font-bold leading-tight">
+              {post.title}
+            </h1>
+            <p className="text-gray-400 text-sm lg:text-xs xl:text-sm leading-tight">
+              {post.text}
+            </p>
+            <div className="flex flex-row justify-between justify-self-end">
+              <div className="flex flex-row gap-1 items-center mr-2">
+                <img src="/like.svg"></img>
+                <p className="text-gray-400 text-xs mt-[4px]">{post.likes}</p>
+              </div>
+              {post.tags && (
+                <div className="flex flex-row gap-2 w-full flex-wrap justify-end bottom-0">
+                  {post.tags.map((tag, index) => (
+                    <div
+                      key={index}
+                      className="px-2 py-1 bg-sky-100 rounded-xl justify-center items-center"
+                    >
+                      <div className="text-blue-500 text-2xs">
+                        {tag}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default BlogItemSkeleton;
