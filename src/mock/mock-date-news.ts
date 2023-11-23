f075eb34b6a7923ea2644637a655533cc95e60f7
lincode <lincode@bk.ru>
2023-11-23 16:27:23 +0300

    fixed mock date

--- src/mock/mock-date-news.ts
+++ src/mock/mock-date-news.ts
@@ -1,60 +1,25 @@
-interface ImageInfo {
-    url: string;
-    copyright: string;
-}
-
-interface News {
-    image: ImageInfo;
-    title: string;
-    author: string;
-    tags: string[];
-}
-
-interface NewsProps { 
-   news: News;
-}
-
-interface NewsBlock2Props {
-    news: News[];
-    title: string;
-    subtitle: string;
-    text: string;
-}
-
-export const NewsBlock1: NewsProps = {
-    news: {
+const Post = {
+    description: {
         image: {
-            url: 'https://images.unsplash.com/photo-1519041266000-208397329799?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-            copyright: 'Photo by NAME on Unsplash'
+            url: '',
+            copyright: ''
         },
-        title: 'News Block 1',
-        author: 'NAME',
-        tags: ['News', 'Update', 'Daily']
-    }      
-}
+        title: '',
+        author: '',
+        tag: ['']
+    },
 
-export const NewsBlock2: NewsBlock2Props = {
-    news: [
+    content: [
         {
-            image: {
-                url: 'https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-                copyright: 'Photo by NAME on Unsplash'
-            },
-            title: 'News Block 2',
-            author: 'NAME',
-            tags: ['Global', 'Economy', 'Markets']
-        },
-        {
-            image: {
-                url: 'https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-                copyright: 'Photo by ANOTHER NAME on Unsplash'
-            },
-            title: 'News Block 3',
-            author: 'ANOTHER NAME',
-            tags: ['Technology', 'Innovation', 'Future']
+            titleType: false,
+            title: '',
+            text: '',
+            images: [
+                {
+                    url: '',
+                    copyright: ''
+                }
+            ]
         }
-    ],
-    title: 'Multiple News Blocks',
-    subtitle: 'A compilation of the latest news',
-    text: 'Here you can find the latest updates on various topics around the world...'
+    ]
 }
